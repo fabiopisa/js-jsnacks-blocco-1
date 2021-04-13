170e435206e1e1d97e54457021fcffad1a5928d1
fabiopisa <pisanellifabio@gmail.com>
2021-04-13 17:16:47 +0200

    commit es JS9 fatto

---
+++ JSnack9/assets/js/script.js
@@ -0,0 +1,12 @@
+var somma = 0;
+var media = 0;
+
+for(var i=1; i<=10; i++){
+  var cifra = [i];
+  console.log(cifra);
+  somma += parseInt(cifra)
+  media = somma/[i];
+}
+
+console.log(somma)
+console.log(media)
--- tuttiscript.js
+++ tuttiscript.js
@@ -110,3 +110,17 @@ while(potenza < limite_somma){
  counter_potenza++;
 }
 /* /settimo JSnack */
+
+/* ottavo JSnack */
+var numero_utente = prompt("digita un numero di 4 cifre");
+ 
+var somma = 0;
+
+for (var i = 0; i < numero_utente.length; i++){
+  var cifra = numero_utente[i];
+  console.log(cifra);
+  somma = somma + parseInt(cifra);
+};
+
+console.log("La somma delle 4 cifre è " + somma)
+/* /ottavo JSnack */
