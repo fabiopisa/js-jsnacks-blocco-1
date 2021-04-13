5f071971be95b2f8d4579606dab7057aac1ff7b2
fabiopisa <pisanellifabio@gmail.com>
2021-04-13 14:59:40 +0200

    commit es JS8

---
+++ JSnack8/assets/js/script.js
@@ -0,0 +1,14 @@
+var numero_utente = parseInt(prompt("digita un numero di 4 cifre"))
+
+var number = numero_utente; 
+var numeri_scomposti = [] ;
+var numero_singolo = number.toString(); 
+
+for (var i = 0, len =  numero_singolo.length; i < len; i += 1){
+  numeri_scomposti.push(+ numero_singolo.charAt(i));
+  console.log(numeri_scomposti);
+  var somma = 
+
+  console.log(somma)
+};
+
