1aedd2672a5abad5d05e695192489d32b40d2a58
fabiopisa <pisanellifabio@gmail.com>
2021-04-13 17:01:46 +0200

    commit JS8 fatto

--- JSnack8/assets/js/script.js
+++ JSnack8/assets/js/script.js
@@ -3,9 +3,9 @@ var numero_utente = prompt("digita un numero di 4 cifre");
 var somma = 0;
 
 for (var i = 0; i < numero_utente.length; i++){
-  var cifra = numero_utente.length[i];
+  var cifra = numero_utente[i];
   console.log(cifra);
-  somma += somma + parseInt(cifra);
-  console.log(somma);
+  somma = somma + parseInt(cifra);
 };
 
+console.log("La somma delle 4 cifre è " + somma)
